839d19dae16fa0cc241cdee44d914e2ccc768b91
wing <895682269@qq.com>
2024-05-08 22:51:32 +0800

    附近商户完结

--- hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
@@ -60,6 +60,20 @@ public class ShopController {
         return shopService.update(shop);
     }
 
+
+//    @GetMapping("/of/type")
+//    public Result queryShopByType(
+//            @RequestParam("typeId") Integer typeId,
+//            @RequestParam(value = "current", defaultValue = "1") Integer current
+//    ) {
+//        // 根据类型分页查询(可以参考这里的分页如何实现)
+//        Page<Shop> page = shopService.query()
+//                .eq("type_id", typeId)
+//                .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+//        // 返回数据
+//        return Result.ok(page.getRecords());
+//    }
+
     /**
      * 根据商铺类型分页查询商铺信息
      * @param typeId 商铺类型
@@ -69,14 +83,11 @@ public class ShopController {
     @GetMapping("/of/type")
     public Result queryShopByType(
             @RequestParam("typeId") Integer typeId,
-            @RequestParam(value = "current", defaultValue = "1") Integer current
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam(value = "x", required = false) Double x,
+            @RequestParam(value = "y", required = false) Double y
     ) {
-        // 根据类型分页查询
-        Page<Shop> page = shopService.query()
-                .eq("type_id", typeId)
-                .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
-        // 返回数据
-        return Result.ok(page.getRecords());
+        return shopService.queryShopByType(typeId, current, x, y);
     }
 
     /**
--- hm-dianping/src/main/java/com/hmdp/dto/ScrollResult.java
+++ hm-dianping/src/main/java/com/hmdp/dto/ScrollResult.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
+// 这里命名为 ScrollDTO 好点
 public class ScrollResult {
     private List<?> list;
     private Long minTime;
